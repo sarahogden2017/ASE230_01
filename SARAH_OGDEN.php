fb51a89c623f9822fb4f265c887ca19dca68d644
Sarah Ogden <sarahogden2017@gmail.com>
2024-08-25 22:44:50 -0400

    added more php tags

--- SARAH_OGDEN.php
+++ SARAH_OGDEN.php
@@ -9,14 +9,14 @@
 <!DOCTYPE html>
 <html lang="en"> 
 <head>
-	<title><?php echo $name ?>'s Resume</title>
+	<title><?php echo $name + "'s Resume" ?></title>
     
     <!-- Meta -->
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Your name's resume">
-    <meta name="author" content="Your name">    
+    <meta name="description" content="Sarah Ogden's resume">
+    <meta name="author" content="Sarah Ogden">    
     <link rel="shortcut icon" href="favicon.ico"> 
     
     <!-- Google Font -->
@@ -336,7 +336,7 @@
     
     <footer class="footer text-center pt-2 pb-5">
 	    <!--/* This template is free as long as you keep the footer attribution link. If you'd like to use the template without the attribution link, you can buy the commercial license via our website: themes.3rdwavemedia.com Thank you for your support. :) */-->
-      <small class="copyright">Designed with <span class="sr-only">love</span><i class="fas fa-heart"></i><?php echo " by Sarah Ogden" ?></small>
+      <small class="copyright"><?php echo "Designed with" ?><span class="sr-only">love</span><i class="fas fa-heart"></i><?php echo " by Sarah Ogden" ?></small>
     </footer>
 
     
